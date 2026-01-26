2b6ca22577cbab8d8c64a19d3f0f4cc3ac72300d
Kevin <marinkevin159@gmail.com>
2026-01-25 22:51:10 -0500

    AboutMe

--- FrontEnd/Cartist-Web/src/pages/AboutMe.jsx
+++ FrontEnd/Cartist-Web/src/pages/AboutMe.jsx
@@ -1,3 +1,6 @@
+import '../styles/pages/AboutMe.css';
+import avatar from '../assets/avatar/Avatar.png';
+
 const AboutMe = () => {
     return (
         <div className="aboutme">
@@ -7,7 +10,7 @@ const AboutMe = () => {
             <p>enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat.</p>     
             <p>Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur.</p>     
             <p>Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.</p>
-            <img src="" alt="" />
+            <img src={avatar} />
         </div>
     )
 }
